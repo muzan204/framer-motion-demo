ef727e2958c822e73ce83c9bcf172595264227aa
gustavo <belchiorgustavo454@gail.com>
2026-05-14 18:49:19 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from 'framer-motion';
+import './styles.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+
+  const [move, setMove] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <motion.h1
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        Framer Motion 🚀
+      </motion.h1>
+
+      {/* BOLINHA */}
+      <motion.div
+        className="box"
+
+        animate={{
+          x: move ? 300 : 0,
+          rotate: 360,
+          scale: [1, 1.2, 1],
+          borderRadius: '50%'
+        }}
+
+        transition={{
+          duration: 1,
+          type: 'spring'
+        }}
+      />
+
+      {/* BOTÃO */}
+      <motion.button
+        className="btn"
+
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+
+        onClick={() => setMove(!move)}
+      >
+        Clique Aqui
+      </motion.button>
+
     </div>
   );
 }
-
-export default App;
